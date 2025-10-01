c7a9b6f831c66b49b0d8a41f76f7a771e8af0b3e
Mohamad-aminn <mohamadeaminn@gmail.com>
2025-10-02 02:29:43 +0400

    set up cookieParser and cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import route from './routes/k8s.js';
 import helmet from 'helmet';
+import morgan from 'morgan';
+import logger from "#configs/logger.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use('/', route);
+
 app.use(helmet());
+app.use(cors({optionsSuccessStatus: 200}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
 
 export default app;
