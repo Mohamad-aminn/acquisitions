0b11a841a1626005afee7fcec426b8831bd90e88
Mohamad-aminn <mohamadeaminn@gmail.com>
2025-10-01 13:14:03 +0400

    set up winston

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import chalk from 'chalk';
 import app from './app.js';
 import 'dotenv/config.js';
+import logger from "./middleware/logger.js";
 
 const port = process.env.PORT || 3000;
-
+logger.error('testing!')
+logger.info('testing!')
 app.listen(port, () => {
   console.log(chalk.blue(`Listening on port ${port}...`));
 });
